4a67ab5f39540cd05953fadedf0ed1805e7a8fce
Amin Rhimi <aminrhimi.1991@gmail.com>
2024-10-16 00:04:46 +0000

    Create home app

--- game_store/urls.py
+++ game_store/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from home.views import test
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', test, name='test'),
 ]
