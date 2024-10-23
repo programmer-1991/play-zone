80415d3c94ba482937df3c346859dd448a8c8f5a
Amin Rhimi <aminrhimi.1991@gmail.com>
2024-10-23 03:21:33 +0000

    Add trusted origins

--- game_store/settings.py
+++ game_store/settings.py
@@ -116,6 +116,10 @@ WSGI_APPLICATION = 'game_store.wsgi.application'
 #}
 DATABASES = { 'default': dj_database_url.parse(os.environ.get("DATABASE_URL")) }
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.ws.codeinstitute-ide.net",
+    "https://*.herokuapp.com"
+]
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
