07094a65554413b95a2dbc6cdb964552e58a3aa3
Amin Rhimi <aminrhimi.1991@gmail.com>
2024-10-27 04:15:43 +0000

    Add templates to home and render a test page

--- game_store/urls.py
+++ game_store/urls.py
@@ -16,10 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from home.views import test
 
 urlpatterns = [
     path("accounts/", include("allauth.urls")),
     path('admin/', admin.site.urls),
-    path('', test, name='test'),
+    path('', include('home.urls')),
 ]
