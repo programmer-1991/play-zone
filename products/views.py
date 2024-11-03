ea7034e1c2da6ac802b015b8704a1d4c1d69e834
Amin Rhimi <aminrhimi.1991@gmail.com>
2024-11-03 05:45:55 +0000

    Add products list view to products app

--- products/views.py
+++ products/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
+
+def products_list(request):
+    """ A view to show all products, including sorting and search queries """
+
+    products = Product.objects.all()
+
+    context = {
+        'products': products,
+    }
+
+    return render(request, 'products/products.html', context)
