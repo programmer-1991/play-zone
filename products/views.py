eae38feb6f7a7b2ca9ef1d3da249fcc97883f6d7
Amin Rhimi <aminrhimi.1991@gmail.com>
2024-11-04 02:08:28 +0000

    Link products list to shop now button and navbar menu

--- products/views.py
+++ products/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render
 from .models import Product
 
 # Create your views here.
 
-def all_products(request):
+def products_list(request):
     """ A view to show all products, including sorting and search queries """
 
     products = Product.objects.all()
@@ -11,17 +11,5 @@ def all_products(request):
     context = {
         'products': products,
     }
-
+    print(products)
     return render(request, 'products/products.html', context)
-
-
-def product_detail(request, product_id):
-    """ A view to show individual product details """
-
-    product = get_object_or_404(Product, pk=product_id)
-
-    context = {
-        'product': product,
-    }
-
-    return render(request, 'products/product_detail.html', context)
