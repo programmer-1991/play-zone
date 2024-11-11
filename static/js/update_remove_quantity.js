0590305242f1443a353ae7411f92f51cf5be2a32
Amin Rhimi <aminrhimi.1991@gmail.com>
2024-11-11 00:54:18 +0000

    Add update and remove functionalities for quantity

--- static/js/update_remove_quantity.js
+++ static/js/update_remove_quantity.js
@@ -1,18 +1,19 @@
-        // Update quantity on click
-        $('.update-link').click(function(e) {
-            var form = $(this).prev('.update-form');
-            form.submit();
-        })
-    
-        // Remove item and reload on click
-        $('.remove-item').click(function(e) {
-            var csrfToken = "{{ csrf_token }}";
-            var itemId = $(this).attr('id').split('remove_')[1];
-            var url = `/bag/remove/${itemId}`;
-            var data = {'csrfmiddlewaretoken': csrfToken, };
-    
-            $.post(url, data)
-             .done(function() {
-                 location.reload();
-             });
-        })
+// Update quantity on click
+$('.update-link').click(function (e) {
+    var form = $(this).prev('.update-form');
+    form.submit();
+})
+
+// Remove item and reload on click
+$('.remove-item').click(function (e) {
+    var csrfToken = $(this).attr('data-token');
+    var itemId = $(this).attr('data-id');
+    var url = `/bag/remove/${itemId}/`;
+    var data = {
+        'csrfmiddlewaretoken': csrfToken,
+    };
+    $.post(url, data)
+        .done(function () {
+            location.reload();
+        });
+})
