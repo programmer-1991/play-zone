37889589087a21a9a4c19c81267a43c88a90efb0
Amin Rhimi <aminrhimi.1991@gmail.com>
2024-11-12 03:35:39 +0000

    Add stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51QK9pHKP7KvyXDEqIPOQfsj9O0cIvX6wuvRUeW6YQz5oREY1sEotsZySZbFTnxVxxhb6x9op89LzIOCcbm2gqMpz00spzjuMcg',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
