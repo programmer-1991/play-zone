e02257ee8592fdc87c780c93fd6a44f0e4f79d0b
Amin Rhimi <aminrhimi.1991@gmail.com>
2024-11-13 18:11:23 +0000

    Update code in StripeWH Handler class

--- game_store/settings.py
+++ game_store/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 
 import os
 import dj_database_url
-if os.path.isfile('env.py'): 
+if os.path.isfile('env.py'):
     import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
