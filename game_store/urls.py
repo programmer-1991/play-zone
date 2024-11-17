925dd1842c32e7c39821e879eda437577c95aa3b
Amin Rhimi <aminrhimi.1991@gmail.com>
2024-11-17 22:19:39 +0000

    Add 404 not found page

--- game_store/urls.py
+++ game_store/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from .views import handler404
 
 urlpatterns = [
     path("accounts/", include("allauth.urls")),
@@ -26,4 +27,6 @@ urlpatterns = [
     path('products/', include('products.urls')),
     path('bag/', include('bag.urls')),
     path('checkout/', include('checkout.urls')),
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+handler404 = 'game_store.views.handler404'
