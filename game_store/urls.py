fd89635389cfb6397f70e3bbf10e54a30cb0c2d2
Amin Rhimi <aminrhimi.1991@gmail.com>
2024-11-19 00:20:47 +0000

    Center contact form and it's headlines

--- game_store/urls.py
+++ game_store/urls.py
@@ -28,6 +28,5 @@ urlpatterns = [
     path('bag/', include('bag.urls')),
     path('checkout/', include('checkout.urls')),
     path('contact/', include('contact.urls')),
-
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 handler404 = 'game_store.views.handler404'
