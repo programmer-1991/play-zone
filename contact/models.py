1be073d3321ccc310417d3565052cd7e0e8d2234
Amin Rhimi <aminrhimi.1991@gmail.com>
2024-12-05 11:24:21 +0000

    Change phone field in contact to integerfield

--- contact/models.py
+++ contact/models.py
@@ -6,7 +6,7 @@ from django.db import models
 class Contact(models.Model):
     name = models.CharField(max_length=254)
     email = models.EmailField(max_length=30, null=True, blank=True)
-    phone = models.CharField(max_length=30, null=True, blank=True)
+    phone = models.IntegerField(null=True, blank=True)
     topic = models.CharField(max_length=30, null=True)
     message = models.TextField(max_length=254, null=True)
     upload = models.FileField(upload_to="uploads/", null=True, blank=True)
